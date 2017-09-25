70d3e9a9816e2fde77394360c6a1600ae51367c0
wchaney98 <wills.apps@yahoo.com>
2017-09-25 15:24:52 -0600

    Started next iteration

---
+++ Colonize/Assets/Scripts/AbilityMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityMenu : MonoBehaviour
+{
+
+    public GameObject Camera;
+    public GameManager GameManager;
+
+    public void SlowdownAbility()
+    {
+        
+    }
+
+    public void SurplusAbility()
+    {
+
+    }
+
+    public void DestroyVirusesAbility()
+    {
+
+    }
+
+    public void TenTimesAbility()
+    {
+
+    }
+
+    void Start()
+    {
+        GameManager = Camera.GetComponent<GameManager>();
+    }
+}
