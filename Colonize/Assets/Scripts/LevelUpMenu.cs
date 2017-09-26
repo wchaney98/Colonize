1655812f421449ade70756c36175b14a053a40af
wchaney98 <wills.apps@yahoo.com>
2017-09-26 17:01:47 -0600

    Basic node level implementation

--- Colonize/Assets/Scripts/LevelUpMenu.cs
+++ Colonize/Assets/Scripts/LevelUpMenu.cs
@@ -1,34 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelUpMenu : MonoBehaviour
 {
     public GameObject Camera;
     public GameManager GameManager;
 
+    public GameObject LevelOneObject;
+    public GameObject LevelTwoObject;
+    public GameObject LevelThreeObject;
+
+    private Button[] LevelUpButtons = new Button[3];
+
     public void LevelUp1()
     {
-
+        GameManager.SelectedNode.Level = 2;
+        GameManager.SelectedNode.Life -= 20;
     }
 
     public void LevelUp2()
     {
-
+        GameManager.SelectedNode.Level = 3;
+        GameManager.SelectedNode.Life -= 30;
     }
 
     public void LevelUp3()
     {
-
+        GameManager.SelectedNode.Level = 4;
+        GameManager.SelectedNode.Life -= 40;
     }
 
     void Start()
     {
         GameManager = Camera.GetComponent<GameManager>();
+        LevelUpButtons[0] = LevelOneObject.GetComponent<Button>();
+        LevelUpButtons[1] = LevelTwoObject.GetComponent<Button>();
+        LevelUpButtons[2] = LevelThreeObject.GetComponent<Button>();
     }
 
     private void Update()
     {
-        // Check if level requirements are met
+        INode node = GameManager.SelectedNode;
+        if (node != null && node.Level <= 3)
+        {
+            foreach (Button b in LevelUpButtons)
+            {
+                b.interactable = false;
+            }
+            LevelUpButtons[node.Level - 1].interactable = true;
+        }
+        else
+        {
+            foreach (Button b in LevelUpButtons)
+            {
+                b.interactable = false;
+            }
+        }
     }
 }
