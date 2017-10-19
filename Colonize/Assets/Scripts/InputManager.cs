ee0e73d99a9770b13487bfc519a6ca781309c65e
wchaney98 <wills.apps@yahoo.com>
2017-10-19 14:11:48 -0600

    Added Singleton generic

--- Colonize/Assets/Scripts/InputManager.cs
+++ Colonize/Assets/Scripts/InputManager.cs
@@ -2,26 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour {
-
-    public static InputManager existing;
-    public float Time { get; set; }
-    public bool TenTimesAbilityActive { get; set; }
-
-    public Dictionary<string, string> Controls;
-
-    void Awake()
+public class InputManager : SingletonBehavior<InputManager>
+{
+    private void Start()
     {
-        if (existing == null)
-        {
-            DontDestroyOnLoad(gameObject);
-            existing = this;
-
-            
-        }
-        else if (existing != this)
-        {
-            Destroy(gameObject);
-        }
+        //
     }
 }
---
+++ Colonize/Assets/Scripts/SingletonBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class SingletonBehavior<T> : MonoBehaviour where T : Component
+{
+    protected static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    instance = obj.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            Debug.Log(typeof(T).Name + " was null. Instancing...");
+            instance = this as T;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
