f479d9e3768a53f5349b03a5b0290b9af2f17595
wchaney98 <wills.apps@yahoo.com>
2017-10-22 21:20:57 -0600

    these too

--- Colonize/Assets/Scripts/InputCommand.cs
+++ Colonize/Assets/Scripts/InputCommand.cs
@@ -1,35 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 public class InputCommand
 {
     public string KeyboardInput;
     public string ControllerInput;
 
+    public delegate bool CheckInput();
+
     public event CheckInput CheckEvent;
     private CheckInput checkEvent;
-
-    public delegate bool CheckInput();
 
     /// <summary>
     /// 
     /// </summary>
+    /// <param name="commands">list of commands</param>
     /// <param name="inputString">axes, keycodes, input...</param>
     /// <param name="isController">controller control?</param>
     /// <param name="checkEvent">how to check if input is true</param>
-    public InputCommand(string keyboardInput, string controllerInput, CheckInput checkEvent)
+    public InputCommand(List<InputCommand> commands, string keyboardInput, string controllerInput, CheckInput checkEvent)
     {
         KeyboardInput = keyboardInput;
         ControllerInput = controllerInput;
         this.checkEvent = checkEvent;
         CheckEvent += this.checkEvent;
-    }   
+        commands.Add(this);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="commands">list of commands</param>
+    /// <param name="inputString">axes, keycodes, input...</param>
+    /// <param name="isController">controller control?</param>
+    /// <param name="checkEvent">how to check if input is true</param>
+    public InputCommand(List<InputCommand> commands, string keyboardInput, string controllerInput)
+    {
+        KeyboardInput = keyboardInput;
+        ControllerInput = controllerInput;
+        checkEvent = new CheckInput(() =>
+        {
+            return Input.GetKeyDown(KeyboardInput) ||
+            Input.GetButtonDown(ControllerInput);
+        });
+        CheckEvent += checkEvent;
+        commands.Add(this);
+    }
 
     public void ModifyCheck(CheckInput newCheckEvent)
     {
         CheckEvent -= checkEvent;
         checkEvent = newCheckEvent;
+        CheckEvent += checkEvent;
+    }
+
+    public void ModifyCheck()
+    {
+        CheckEvent -= checkEvent;
+        checkEvent = new CheckInput(() =>
+        {
+            return KeyboardInput.Length > 0 ? Input.GetKeyDown(KeyboardInput) : false ||
+            Input.GetButtonDown(ControllerInput);
+        });
         CheckEvent += checkEvent;
     }
 
--- Colonize/Assets/Scripts/InputManager.cs
+++ Colonize/Assets/Scripts/InputManager.cs
@@ -4,30 +4,124 @@ using UnityEngine;
 
 public class InputManager : SingletonBehavior<InputManager>
 {
-    InputCommand AKey;
-    
-    private void Start()
+    public InputCommand ConnectKey;
+    public InputCommand LevelUpKey;
+    public InputCommand SlowTimeKey;
+    public InputCommand SurplusKey;
+    public InputCommand DestroyVirusesKey;
+    public InputCommand TenTimesResourcesKey;
+
+    public InputCommand ControllerSelectClosest;
+    public InputCommand ControllerMoveSelected;
+
+    public int SelectedLayout;
+
+    public List<InputCommand> GetCommands()
     {
-        AKey = new InputCommand("a", "J_A", () =>
+        return commands;
+    }
+
+    public void ApplyLayout1()
+    {
+        ConnectKey.ControllerInput = "J_X";
+
+        LevelUpKey.ControllerInput = "J_Y";
+
+        ControllerSelectClosest.ControllerInput = "J_A";
+        ControllerSelectClosest.ModifyCheck();
+
+        ControllerMoveSelected.ControllerInput = "J_Triggers";
+        ControllerMoveSelected.ModifyCheck(() =>
         {
-            return Input.GetKeyDown(AKey.KeyboardInput) ||
-                   Input.GetButtonDown(AKey.ControllerInput);
+            return Input.GetAxis(ControllerMoveSelected.ControllerInput) < -0.1f;
         });
-        AKey.KeyboardInput = "b";
-        AKey.ModifyCheck(() =>
+
+        SelectedLayout = 1;
+    }
+
+    public void ApplyLayout2()
+    {
+        ConnectKey.ControllerInput = "J_X";
+
+        LevelUpKey.ControllerInput = "J_Y";
+
+        // Right trigger
+        ControllerSelectClosest.ControllerInput = "J_Triggers";
+        ControllerSelectClosest.ModifyCheck(() =>
         {
-            return Input.GetKeyUp(AKey.KeyboardInput) ||
-                   Input.GetButtonDown(AKey.ControllerInput);
+            return Input.GetAxis(ControllerSelectClosest.ControllerInput) < -0.1f;
         });
+
+        // Left trigger
+        ControllerMoveSelected.ControllerInput = "J_Triggers";
+        ControllerMoveSelected.ModifyCheck(() =>
+        {
+            return Input.GetAxis(ControllerMoveSelected.ControllerInput) > 0.1f;
+        });
+
+        SelectedLayout = 2;
+    }
+
+    public void ApplyLayout3()
+    {
+        ConnectKey.ControllerInput = "J_RBumper";
+
+        LevelUpKey.ControllerInput = "J_Y";
+
+        ControllerSelectClosest.ControllerInput = "J_X";
+        ControllerSelectClosest.ModifyCheck();
+
+        ControllerMoveSelected.ControllerInput = "J_Triggers";
+        ControllerMoveSelected.ModifyCheck(() =>
+        {
+            return Input.GetAxis(ControllerMoveSelected.ControllerInput) < -0.1f;
+        });
+
+        SelectedLayout = 3;
+    }
+
+    private List<InputCommand> commands = new List<InputCommand>();
+
+    protected override void Init()
+    {
+        ConnectKey = new InputCommand(commands, "c", "J_X");
+        LevelUpKey = new InputCommand(commands, "l", "J_Y");
+
+        SlowTimeKey = new InputCommand(commands, "r", "J_DpadHorizontal", () =>
+        {
+            return Input.GetKeyDown(SlowTimeKey.KeyboardInput) ||
+            Input.GetAxis(SlowTimeKey.ControllerInput) == -1;
+        });
+        SurplusKey = new InputCommand(commands, "t", "J_DpadHorizontal", () =>
+        {
+            return Input.GetKeyDown(SurplusKey.KeyboardInput) ||
+            Input.GetAxis(SurplusKey.ControllerInput) == 1;
+        });
+        DestroyVirusesKey = new InputCommand(commands, "f", "J_DpadVertical", () =>
+        {
+            return Input.GetKeyDown(DestroyVirusesKey.KeyboardInput) ||
+            Input.GetAxis(DestroyVirusesKey.ControllerInput) == 1;
+        });
+        TenTimesResourcesKey = new InputCommand(commands, "g", "J_DpadVertical", () =>
+        {
+            return Input.GetKeyDown(TenTimesResourcesKey.KeyboardInput) ||
+            Input.GetAxis(TenTimesResourcesKey.ControllerInput) == -1;
+        });
+
+        ControllerSelectClosest = new InputCommand(commands, "", "J_A", () =>
+        {
+            return Input.GetButtonDown(ControllerSelectClosest.ControllerInput);
+        });
+        ControllerMoveSelected = new InputCommand(commands, "", "J_Triggers", () =>
+        {
+            return Input.GetAxis(ControllerMoveSelected.ControllerInput) < -0.1f;
+        });
+
+        SelectedLayout = 1;
     }
 
     private void Update()
     {
-        Debug.Log(AKey.Check());
-    }
 
-    private bool CheckAKey()
-    {
-        return Input.GetKeyDown(KeyCode.A);
     }
 }
